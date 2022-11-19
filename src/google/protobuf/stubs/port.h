a70adc0f01198bd3087e2cdc3c9dee9c477dcc32
JCash <mwesterdahl76@gmail.com>
2022-11-19 14:48:38 +0100

    Added DM_PROTOBUF_USE_MACHINE_ENDIAN_H

--- src/google/protobuf/stubs/port.h
+++ src/google/protobuf/stubs/port.h
@@ -57,7 +57,7 @@
 #pragma runtime_checks("c", off)
 #endif
 #else
-#ifdef __APPLE__
+#if defined(__APPLE__) || defined(DM_PROTOBUF_USE_MACHINE_ENDIAN_H)
 #include <machine/endian.h>  // __BYTE_ORDER
 #elif defined(__FreeBSD__)
 #include <sys/endian.h>  // __BYTE_ORDER
